e5040cf4679c68527030535f4f4e5fa95c5e3fbd
datalyst1127 <96359837+datalyst1127@users.noreply.github.com>
2023-04-20 18:39:53 -0700

    provided answer

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function checkValue() {
-	// Get the input value
-	var value = document.getElementById("input").value;
-	
-	// TODO: Check if the value is truthy or falsy
-	if ("ADD YOUR LOGIC EXPRESSION HERE"){
-		document.getElementById("result").innerHTML = value + " is falsy!";
-        document.getElementById("result").style.color = "red"
-    } else {
-		document.getElementById("result").innerHTML = value + " is truthy!";
-	}
+  // Get the input value
+  var value = document.getElementById("input").value;
+
+  let isFalsy =
+    value == "false" ||
+    value === "" ||
+    value == "NaN" ||
+    value == "null" ||
+    value == 0 ||
+    value == "undefined";
+  // TODO: Check if the value is truthy or falsy
+  if (isFalsy) {
+    document.getElementById("result").innerHTML = value + " is falsy!";
+    document.getElementById("result").style.color = "red";
+  } else {
+    document.getElementById("result").innerHTML = value + " is truthy!";
+  }
 }
